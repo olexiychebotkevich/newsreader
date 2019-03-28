a4cfd733a270e2c53c7b7bc5bd3389044a64893c
olexiychebotkevich <olexiychebotkevich@A33COMP4>
2019-03-28 16:54:36 +0200

    added styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {NewsService} from './news.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'newreader';
+export class AppComponent implements OnInit {
+  mArticles:Array<any>;
+  mSources:Array<any>;
+
+  ngOnInit(): void {
+   this.news.initArticles().subscribe(data=>this.mArticles=data['articles']);
+   this.news.initSources().subscribe(data=>this.mSources=data['sources']);
+
+  }
+ 
+  searchArticles(sources:string){
+    this.news.getArticlesById(sources).subscribe(data=>this.mArticles=data['articles']);
+  }
+  constructor(private news:NewsService) {}
 }
--- src/app/news.service.ts
+++ src/app/news.service.ts
@@ -1,10 +1,26 @@
 import { Injectable } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http'
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService {
 apiKey='638c3f49059b4093b611faac02f905b0';
 apiUrl='https://newsapi.org/v2/'
-  constructor() { }
+  constructor(private client:HttpClient) {
+
+   }
+
+   initSources(){
+     return this.client.get(this.apiUrl+'sources?/language=en&apiKey='+this.apiKey);
+   }
+
+
+   initArticles(){
+    return this.client.get(this.apiUrl+'top-headlines?sources=techcrunch&apiKey='+this.apiKey);
+   }
+
+
+   getArticlesById(source:string){
+    return this.client.get(this.apiUrl+'top-headlines?sources='+source+'&apiKey='+this.apiKey);
+   }
 }
